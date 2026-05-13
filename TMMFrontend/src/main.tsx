cd7998fb99ce1e2ab4cececaf9d3e0e1f06eebcf
Franz Hiebler <Franz@hiebler.net>
2026-05-13 16:40:29 +0200

    Frontend: CSS in Bereiche aufteilen"

--- TMMFrontend/src/main.tsx
+++ TMMFrontend/src/main.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
+
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/forms.css";
+import "./styles/messages.css";
+import "./styles/games.css";
+import "./styles/locations.css";
+import "./styles/modals.css";
+import "./styles/responsive.css";
+
 import { UserProvider } from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
